d489719530faff0d7ba0268905a934f951cda8df
gustavo <gustavo.c.jesus@aluno.senai.br>
2024-12-02 09:41:30 -0500

    atividade

---
+++ revisao.py
@@ -0,0 +1,10 @@
+#ler entradas do usuarios
+nome = input()#armazenar o nome do aluno
+nota1 = float(input())#ler 4 notas do aluno
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+
+faltas = int(input())#calculo da media
+media = (nota1+nota2+nota3+nota4)/4
+print(media)
