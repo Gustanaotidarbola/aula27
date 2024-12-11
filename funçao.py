b16e79a4e02cd7a04c1988235d8b5b6a06dbec44
gustavo <gustavo.c.jesus@aluno.senai.br>
2024-12-11 10:01:08 -0500

    para salvar alteração do horario

--- funçao.py
+++ funçao.py
@@ -1,15 +1,16 @@
 #declarar uma função
-def bom_dia():
-    print("bom dia!")
-    
-#declarar uma função
-def boa_noite():
-    print("boa noite!")
-
-#declarar função
-def boa_tarde():
-    print("boa tarde!")
-
-bom_dia()
-boa_noite()
-boa_tarde()
+def saudacoes(hora_do_dia): #exibir a saudação correspondente a hora do dia
+    #dar bom dia
+    if (hora_do_dia >= 0) and (hora_do_dia <= 12):
+        print("bom dia!")
+    #dar boa tarde
+    elif (hora_do_dia >= 13) and (hora_do_dia <=18):
+        print("boa tarde!")
+    #dar boa noite
+    elif (hora_do_dia >= 18) and (hora_do_dia <=23):
+        print("boa noite!")
+        
+#peço para  usuario dizer a hora atual
+resposta = int(input("digite que horas são:\n"))
+#chama a função passando para ela o parametro obrigatorio
+saudacoes(resposta)
