cac004e1aa671bc93372a6c5f296a1b291939c84
gustavo <gustavo.c.jesus@aluno.senai.br>
2024-12-11 08:47:17 -0500

    alteração para salvar

---
+++ funçao.py
@@ -0,0 +1,15 @@
+#declarar uma função
+def bom_dia():
+    print("bom dia!")
+    
+#declarar uma função
+def boa_noite():
+    print("boa noite!")
+
+#declarar função
+def boa_tarde():
+    print("boa tarde!")
+
+bom_dia()
+boa_noite()
+boa_tarde()
